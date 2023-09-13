a674efd89cc1b3e6fdd2407bc1294597e641f5ed
Admin <Admin@DESKTOP-BMU23NL>
2023-09-13 19:32:18 +0300

    Добавьте файлы проекта.

---
+++ python/python.py
@@ -0,0 +1,8 @@
+# Вывод "Khal Drogo's favorite word is "athjahakar""
+print("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+
+# Вывод в столбик
+print("Gregor Clegane\nDunsen\nPolliver\nChiswyck")
+
+#Вывод Did Joffrey agree?
+print(" - Did Joffrey agree?\n - He did. He also said \"I  love using \\n\"")
